dcac0c86d38e64c7ea1b747a272520deb68388f5
Thai Hua <huavietthai299@gmail.com>
2025-11-05 22:11:56 +0700

    Add: Camera Follow Player; Edit: Graphic in Level 1 (Room 1, 2)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Room Camera
+    [SerializeField] private float speed = 0.5f;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+
+    // Follow Player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance = 2;
+    [SerializeField] private float cameraSpeed = 0.5f;
+    private float lookAhead;
+
+    private void Update()
+    {
+        // Room Camera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        // Follow Player
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+    }
+}
