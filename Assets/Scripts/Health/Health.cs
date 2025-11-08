9f248e65efc687b718846aedc6e9ce12c6f632b4
Thai Hua <huavietthai299@gmail.com>
2025-11-09 02:20:47 +0700

    Add: Prefab HealthCollectable & Saw, Player & Enemy Layer, IFrames; Edit: Fix Animation Controller

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,16 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth = 3;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool isDead;
 
+    [Header("IFrames")]
+    [SerializeField] private float iFramesDuration = 2;
+    [SerializeField] private int numberOfFlashes = 3;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -20,7 +28,7 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
-            //iframes
+            StartCoroutine(Invunerability());
         } else
         {
             if (!isDead)
@@ -35,5 +43,18 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(8, 9, false);
     }
 }
