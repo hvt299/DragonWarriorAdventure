ef27494d3a6148ba32b933e021849d617a0ad3a2
giodong34567 <pacman16012004@gmail.com>
2025-11-20 13:10:12 +0700

    feat: Thêm màn hình game over: Restart, main menu, quit

---
+++ Assets/Scripts/UI/SelectionArrow.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionArrow : MonoBehaviour
+{
+    [SerializeField] private RectTransform[] options;
+    [SerializeField] private AudioClip changeSound;
+    [SerializeField] private AudioClip interactSound;
+    [SerializeField] private float offsetX = -20f;
+
+    private RectTransform rect;
+    private int currentPosition;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            ChangePosition(-1);
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            ChangePosition(1);
+
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.E))
+            Interact();
+    }
+
+    private void ChangePosition(int change)
+    {
+        if (options.Length == 0) return;
+
+        currentPosition += change;
+
+        // Wrap index
+        if (currentPosition < 0)
+            currentPosition = options.Length - 1;
+        else if (currentPosition >= options.Length)
+            currentPosition = 0;
+
+        // Play move sound
+        if (change != 0 && SoundManager.instance != null)
+            SoundManager.instance.PlaySound(changeSound);
+
+        // Move arrow
+        Vector3 pos = options[currentPosition].localPosition;
+        pos.x = pos.x + offsetX;     // không cộng dồn
+        rect.localPosition = pos;
+    }
+
+    private void Interact()
+    {
+        if (SoundManager.instance != null)
+            SoundManager.instance.PlaySound(interactSound);
+
+        Button btn = options[currentPosition].GetComponent<Button>();
+        if (btn != null)
+            btn.onClick.Invoke();
+    }
+}
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private AudioClip gameOverSound;
+
+    private void Awake()
+    {
+        gameOverScreen.SetActive(false); // Make sure game over screen is hidden at start
+    }
+
+    public void GameOver()
+    {
+
+        // Change position of the selection arrow
+        gameOverScreen.SetActive(true);
+        SoundManager.instance.PlaySound(gameOverSound);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false; // out game
+    }
+}
